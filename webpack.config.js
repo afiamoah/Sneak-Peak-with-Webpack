e10b3741fa3174c4d625ae61cbcc5a1e4926cbeb
afiamoah <afiamoah90@gmail.com>
2022-08-24 17:08:35 -0700

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,5 @@
-// const path = require("path");
-
-// module.exports = {
-//   entry: { index: path.resolve(__dirname, "source", "index.js") }
-// }
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// module.exports = {
-//     output: {
-//       path: path.resolve(__dirname, "build")
-//     }
-//   };
 
 module.exports = {
   module: {
