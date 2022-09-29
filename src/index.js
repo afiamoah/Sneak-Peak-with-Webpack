5bf985d870983e15338bcecbebed73f6ee4447cc
afiamoah <afiamoah90@gmail.com>
2022-09-29 16:40:16 +0000

    local storage created,data saved and retrieved check index,Js file

--- src/index.js
+++ src/index.js
@@ -1,38 +1,27 @@
 import "./style.css";
-const DisplayItems = document.querySelector('.DisplayItems');
-const Items = document.querySelector('.NewItem');
-const ItemsContainer = [
-  {
-    id: 1,
-    Description: 'Play FootBall',
-    Completed: false,
-  },
-  {
-    id: 2,
-    Description: 'Watch Movies',
-    Completed: false,
-  },
-  {
-    id: 3,
-    Description: 'Go for Party',
-    Completed: false,
-  },
-  {
-    id: 4,
-    Description: 'morining section meeting',
-    Completed: false,
-  },
-];
+const DisplayItems=document.querySelector('.DisplayItems')
+const GetItems=localStorage.getItem('Todo')
+const DeleteItem=document.querySelectorAll('.RemoveItems')
+// const DeleteItems=document.querySelector('.')
+const Items=document.querySelector('.NewItem')
+let ItemsContainer=[]
+let holdItems={}
+
 const Validate = () => {
-  if (Items.value !== '') {
-    return true;
-  }
-  return false;
-};
-const ShowAllItems = () => {
-  DisplayItems.innerHTML = '';
-  ItemsContainer.forEach((item, index) => {
-    DisplayItems.innerHTML += `
+if(Items.value !==''){
+   return true;
+}else{
+   return false;
+}
+}
+
+const ShowAllItems = () =>{
+   DisplayItems.innerHTML='';
+   if(GetItems !== null){
+    const AllItems=JSON.parse(localStorage.getItem('Todo'))
+    ItemsContainer=AllItems;
+    AllItems.forEach((item,index) => {
+       DisplayItems.innerHTML+= `
        <div class="TodoList">
        <div class="form">
     <input type="checkbox" placeholder="..." class="cancelItem" id="${index}"> 
@@ -40,57 +29,63 @@ const ShowAllItems = () => {
    </div>
    <span class="RemoveItems" button id="${index}"><i  id="del+${index}" class="fa fa-trash" class="del" name="del" aria-hidden="true"></i><i id="edit+${index}" class="fas fa-ellipsis-v"></i></span>
    </div>
-           `;
-  });
-};
+           `  
+    });
+   }else{
+       []
+   }
+}
+
 const ShowItems = () => {
-  DisplayItems.innerHTML = '';
-  ItemsContainer.forEach((item, index) => {
-    DisplayItems.innerHTML += `
-       <div class="TodoList">
-       <div class="form">
-    <input type="checkbox" placeholder="..." class="cancelItem" id="${index}"> 
-    <input type="text" class="list-items" value="${item.Description}" id="${index}">
-   </div>
-   <span class="RemoveItems" button id="${index}"><i  id="del+${index}" class="fa fa-trash" class="del" name="del" aria-hidden="true"></i><i id="edit+${index}" class="fas fa-ellipsis-v"></i></span>
-   </div>
-           `;
-  });
-};
-const DisplayAllItems = () => {
-  Items.addEventListener('keypress', (event) => {
-    if (event.key == 'Enter') {
-      if (Validate()) {
-        ShowItems();
-      }
-    }
-  });
-};
-const ChangeColor = () => {
-  document.querySelectorAll('.list-items').forEach((container) => {
-    container.addEventListener('focus', (event) => {
-      document.querySelectorAll('.TodoList').forEach((Item, index) => {
-        if (event.target.id === index) {
-          Item.style.background = 'lightyellow';
-          document.getElementById(`del+${index}`).style.visibility = 'visible';
-          document.getElementById(`edit+${index}`).style.visibility = 'hidden';
-        }
-      });
-    });
-  });
-  document.querySelectorAll('.list-items').forEach((container) => {
-    container.addEventListener('focusout', (event) => {
-      document.querySelectorAll('.TodoList').forEach((Item, index) => {
-        if (event.target.id == index) {
-          Item.style.background = 'white';
-          document.getElementById(`del+${index}`).style.visibility = 'hidden';
-          document.getElementById(`edit+${index}`).style.visibility = 'visible';
-        }
-      });
-    });
-  });
-};
+   window.location.reload();
+   DisplayItems.innerHTML='';
+   const AllItems=JSON.parse(localStorage.getItem('Todo'))
+   ItemsContainer=AllItems;
+   ItemsContainer.forEach((item,index) => {
+   DisplayItems.innerHTML+= `
+   <div class="TodoList">
+   <div class="form">
+<input type="checkbox" placeholder="..." class="cancelItem" id="${index}"> 
+   <input type="text" class="list-items" value="${item.Description}" id="${index}">
+</div>
+   <span class="RemoveItems" button id="${index}"><i  id="del+${index}" class="fa fa-trash" aria-hidden="true"></i><i id="edit+${index}" class="fas fa-ellipsis-v"></i></span>
+</div>` 
+    });   
+}
+
+  
+
+
+const SaveItems= ()=>{
+holdItems = {
+id:1,
+Description:Items.value,
+Completed:false
+}
+ItemsContainer.push(holdItems)
+localStorage.setItem('Todo',JSON.stringify(ItemsContainer))
+}
+
+const DisplayAllItems = () =>{
+   Items.addEventListener('keypress',(event)=>{
+   if(event.key == 'Enter'){
+       if(Validate()){
+           SaveItems();
+           ShowItems();
+   }
+   }
+   })
+}
+
+
+
 
 DisplayAllItems();
 ShowAllItems();
-ChangeColor();
+RemoveItem()
+CancelTask()
+UncheckTask()
+ClearAllCompletedTask();
+ChangeColor()
+EditItems();
+
